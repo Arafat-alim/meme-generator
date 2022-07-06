d1c34b69ab2f5bc0b9f91f373d12e40ec9d806cf
arafat-alim <arafat97alim@gmail.com>
2022-07-06 21:06:46 +0530

    navbar or Header component is created and styled successfully.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import logo from "../images/troll-face.png";
 export default function NavBar() {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <nav>
+      <img src={logo} alt="meme--logo" className="nav--image" />
+      <h1 className="nav--title">Meme Generator</h1>
+      <h3 className="nav--project">React Project 3 - Meme Generator</h3>
+    </nav>
   );
 }
