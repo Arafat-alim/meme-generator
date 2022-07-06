aae044ab574dee6a6ef9fbaeee2e59feddfe7797
arafat-alim <arafat97alim@gmail.com>
2022-07-06 23:51:23 +0530

    meme body component created.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import Name from "./components/Name";
+import Meme from "./components/Meme";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Name />
+      <Meme />
     </div>
   );
 }
